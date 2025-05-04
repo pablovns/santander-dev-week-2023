019e2dc372c3891aaefaefeaac82f265bef244d1
pablovns <viniciuspablo962@gmail.com>
2025-05-04 17:06:22 -0300

    test: adicionar mock de dados e dependências para testes unitários

--- src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
+++ src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
@@ -1,7 +1,17 @@
 package me.dio;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.dio.domain.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SantanderDevWeek2023ApplicationTests {
